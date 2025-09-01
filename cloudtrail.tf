5cd4b60bc18bd451c7a30971268487f31c5c0c07
Soli <solee_auditore@outlook.com>
2025-09-01 21:13:48 +0200

    fix

--- cloudtrail.tf
+++ cloudtrail.tf
@@ -25,7 +25,7 @@ data "aws_iam_policy_document" "cloudtrail" {
     condition {
       test     = "StringEquals"
       variable = "aws:SourceArn"
-      values   = ["arn:${data.aws_partition.current.partition}:cloudtrail:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:trail/"]
+      values   = ["arn:${data.aws_partition.current.partition}:cloudtrail:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:trail/${aws_cloudtrail.main.name}"]
     }
   }
 
