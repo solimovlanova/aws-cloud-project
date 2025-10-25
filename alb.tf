5f9c68fbcb830f4e225ac61bb48ba6039ac69a82
Soli <solee_auditore@outlook.com>
2025-10-25 09:35:41 +0200

    adding target group and listener to alb

--- alb.tf
+++ alb.tf
@@ -18,8 +18,59 @@ resource "aws_security_group" "alb" {
   vpc_id      = data.aws_vpc.main.id
 
   tags = {
-    Name = "allow_tls"
+    Name = "alb-sg"
   }
 }
+
+resource "aws_security_group_rule" "http-alb" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = [data.aws_vpc.main.cidr_block]
+  security_group_id = aws_security_group.alb[0].id
+}
+
+resource "aws_security_group_rule" "https-alb" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = [data.aws_vpc.main.cidr_block]
+  security_group_id = aws_security_group.alb[0].id
+}
+
+resource "aws_lb_target_group" "application_1" {
+  name     = "tg-application-1"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.main.id
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener" "front_end_app1" {
+  count             = var.create_alb ? 1 : 0
+  load_balancer_arn = aws_lb.application[0].arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.application_1.arn
+  }
+}
+
+
+
+
+
 
 
--- app.tf
+++ app.tf
@@ -3,10 +3,51 @@ resource "aws_instance" "application_1" {
  count = var.create_app1 ? 1 : 0
  ami = "ami-033a3fad07a25c231"
  instance_type = "t2.micro"
+ security_groups = [aws_security_group.app1[0].name]
  subnet_id = data.aws_subnets.public.ids[0]
  tags = {
    Name = "Application_1"
  }
 }
 
+
+resource "aws_lb_target_group_attachment" "application_1" {
+  count            = var.create_app1 ? 1 : 0
+  target_group_arn = aws_lb_target_group.application_1.arn
+  target_id        = aws_instance.application_1[0].id
+  port             = 80
+}
+
+
+resource "aws_security_group" "app1" {
+  count = var.create_alb ? 1 : 0
+  name        = "ec2-sg"
+  description = "Allow traffic to EC2"
+  vpc_id      = data.aws_vpc.main.id
+
+  tags = {
+    Name = "app1-sg"
+  }
+}
+
+resource "aws_security_group_rule" "http-alb-ec2" {
+  count = var.create_alb ? 1 : 0
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  security_group_id = aws_security_group.app1[0].id 
+  source_security_group_id = aws_security_group.alb[0].id
+}
+
+
+resource "aws_security_group_rule" "https-alb-ec2" {
+  count = var.create_alb ? 1 : 0
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  security_group_id = aws_security_group.app1[0].id 
+  source_security_group_id = aws_security_group.alb[0].id
+}
 
